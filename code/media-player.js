007032d6d7d3a73464a678a82e501b803b33fbde
ericholiveira <ericholiveira10@gmail.com>
2015-06-03 19:41:51 -0300

    teste

---
+++ code/media-player.js
@@ -0,0 +1,9 @@
+function changeVideo1() {
+  document.getElementById('video1').src =
+    "https://youtube.com/embed/M-P3l9ezaF8";
+}
+
+function changeVideo2() {
+  document.getElementById('video2').src =
+    "http://player.vimeo.com/video/59749737";
+}
