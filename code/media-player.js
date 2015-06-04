1b76bc534a6ac79703c049dddf6910049953a5ad
ericholiveira <ericholiveira10@gmail.com>
2015-06-03 23:22:37 -0300

    remove unused

--- code/media-player.js
+++
@@ -1,9 +0,0 @@
-function changeVideo1() {
-  document.getElementById('video1').src =
-    "https://youtube.com/embed/M-P3l9ezaF8";
-}
-
-function changeVideo2() {
-  document.getElementById('video2').src =
-    "http://player.vimeo.com/video/59749737";
-}
